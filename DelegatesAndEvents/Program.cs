c182f62ec7e28472e68915619c6bfbe90a4f713a
visualstudio <visualstudio@230-PC08>
2019-03-07 16:08:34 -0500

    commit 06lab

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -12,22 +12,37 @@ namespace DelegatesAndEvents
         {
             DelegateExercises dhammu = new DelegateExercises();
             dhammu.Method3();
+            Console.ReadLine();
         }
     }
     public class DelegateExercises
     {
-        public delegate int MyDelegate();
-        void Method1() {
+        public delegate int MyDelegate(int intValue);
+        int Method1(int intMethod1) {
 
-            System.Console.WriteLine("MyDelegate");
+            return intMethod1 * 2;
         }
 
+        int Method2(int intMethod1)
+        {
+
+            return intMethod1*10;
+        }
+
+        public void Method4(MyDelegate myDelegate)
+        {
+
+            int result = myDelegate(10);
+            Console.WriteLine(result);
+        }
         public void Method3()
         {
 
             MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
+            Method4(myDelegate);
+            myDelegate = new MyDelegate(Method2);
+            Method4(myDelegate);
         }
-        
+
     }
 }
